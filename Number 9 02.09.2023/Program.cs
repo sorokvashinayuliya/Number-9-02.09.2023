f1d0d617793d6b7dd5ef4b0d84420539869553b0
79176 <79176@LAPTOP-MN0I58DS>
2023-09-07 19:29:28 +0400

    Добавьте файлы проекта.

---
+++ Number 9 02.09.2023/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_9_02._09._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Задача 9. Найти общую стоимость покупки*/
+            Console.WriteLine("Введите стоимость 1 кг конфет");
+            int candies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость 1 кг печенья");
+            int cookies = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите стоимость 1 кг яблок");
+            int apples = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите массу конфет");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите массу печенья");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите массу яблок");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Стоимость покупки:" + (candies * x + cookies * y + apples * z) + "кг");
+        }
+    }
+}
---
+++ Number 9 02.09.2023/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Number 9 02.09.2023")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Number 9 02.09.2023")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("0971d2a6-ba6f-4fe2-ae73-eeaa9533cb87")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
